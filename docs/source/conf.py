3eba31d9269d000094b1c8e3dd845029d8c8530d
Andreas Hefti <anhefti@yahoo.de>
2022-12-29 16:26:51 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Firefly'
+project = 'Firefly 2D Game API'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Andreas Hefti'
 
 release = '0.1'
 version = '0.1.0'
